5323bd6e67b32b11254285835ff435e8de2a710c
Vishal Singh Bisht <kickbuttowski@Vishals-MacBook.local>
2023-11-25 09:42:37 +0530

    Added SwiftUIGradientsView

--- HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIGradientsView.swift
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIGradientsView.swift
@@ -5,12 +5,21 @@
 //  Created by Vishal Singh Bisht on 25/11/23.
 //
 
+
 import SwiftUI
 
 struct SwiftUIGradientsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .lineLimit(3)
+       RoundedRectangle(cornerRadius: 20)
+            .fill(
+                AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)), Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1))]), center: .topLeading, angle: .degrees(45 + 180))
+                
+               // RadialGradient(gradient: Gradient(colors: [Color.red, Color.blue]), center: .leading, startRadius: 10, endRadius: 500)
+               // LinearGradient(colors: [Color.red, Color.blue], startPoint: .leading, endPoint: .trailing)
+                // Color.red
+            )
+           // .shadow(color: Color.blue, radius: 10)
+            .frame(width: 300, height: 200, alignment: .center)
     }
 }
 
