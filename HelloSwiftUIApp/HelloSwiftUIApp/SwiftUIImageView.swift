5f2b8c5643a22e908135ecf5e0dffe1f9fc16a04
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-11-25 17:45:57 +0530

    Added SwiftUIImageViewt

---
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIImageView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIImageView.swift
+//  HelloSwiftUIApp
+//
+//  Created by Vishal Singh Bisht on 25/11/23.
+//
+
+import SwiftUI
+
+struct SwiftUIImageView: View {
+    var body: some View {
+        Image(systemName: "paperplane.fill")
+            //.resizable()
+            .font(.largeTitle)
+            //.scaledToFill()
+    }
+}
+
+struct SwiftUIImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIImageView()
+    }
+}
