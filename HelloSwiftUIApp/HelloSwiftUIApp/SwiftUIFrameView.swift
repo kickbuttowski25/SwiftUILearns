f4e2032b33a7d7822c0df7840fce7f3e7ab35d80
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-11-26 10:37:27 +0530

    Modified fran view

--- HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIFrameView.swift
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIFrameView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SwiftUIFrameView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
             .background(Color.red)
             .cornerRadius(25)
             .frame(width: 200, height: 100)
