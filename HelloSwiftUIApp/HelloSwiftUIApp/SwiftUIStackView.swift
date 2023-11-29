ddcc23ddd7fe55ed0f37778c3ff0e08d562a25bf
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-11-29 19:17:37 +0530

    Added SwiftUIStackView

---
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIStackView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIStackView.swift
+//  HelloSwiftUIApp
+//
+//  Created by Vishal Singh Bisht on 29/11/23.
+//
+
+import SwiftUI
+
+struct SwiftUIStackView: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+            Text("World!")
+        }
+    }
+}
+
+struct SwiftUIStackView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIStackView()
+    }
+}
