c390e716a1e1aa62ab6c250cd44f52f934965655
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-12-05 22:42:35 +0530

    created button view and background color Binding

--- HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIBindingView.swift
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIBindingView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct SwiftUIBindingView: View {
+    @State var backgroundColor: Color = .green
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            backgroundColor.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ButtonView(backgroundColor: $backgroundColor)
+            }
+        }
+    }
+}
+
+struct ButtonView: View {
+    @Binding var backgroundColor: Color
+    
+    var body: some View {
+        Button {
+            self.backgroundColor = .yellow
+        } label: {
+            Text("Button")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.blue)
+                .cornerRadius(10)
+        }
     }
 }
 
