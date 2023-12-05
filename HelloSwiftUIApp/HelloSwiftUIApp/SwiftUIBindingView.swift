3e668a2ca0555af6e9b758841294f59351041bb7
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-12-05 22:36:46 +0530

    Added SwiftUIBindingView

---
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIBindingView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIBindingView.swift
+//  HelloSwiftUIApp
+//
+//  Created by Vishal Singh Bisht on 05/12/23.
+//
+
+import SwiftUI
+
+struct SwiftUIBindingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIBindingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIBindingView()
+    }
+}
