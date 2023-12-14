65aec08440557d3a293faffb0513962863f26fb6
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-12-14 21:40:49 +0530

    Added List, Section, on delete and on move functions

--- HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIListView.swift
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIListView.swift
@@ -9,16 +9,87 @@ import SwiftUI
 
 struct SwiftUIListView: View {
     @State var fruits: [String] = ["apple","orange", "banana","peach"]
+    @State var veggies: [String] = ["Carrot", "Cabbage","Potato","Tomato"]
+    
     var body: some View {
-        List {
-            Section {
-                ForEach(fruits, id: \.self) { fruit in
-                    Text(fruit.capitalized)
+        NavigationView {
+            List {
+                Section(header:
+                            HStack {
+                    Text("Fruits")
+                    Image(systemName: "flame.fill")
                 }
-            } header: {
-                Text("Fruits")
+                    .foregroundColor(.blue)
+                    .font(.largeTitle)
+
+                
+                ) {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit.capitalized)
+                        
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move(from:to:))
+                    .listRowBackground(Color.orange) 
+                    
+                }
+                Section(header:
+                            HStack {
+                    Text("Veggies")
+                    Image(systemName: "heart.fill")
+                }
+                    .foregroundColor(.red)
+                    .font(.largeTitle)
+                
+                ) {
+                    ForEach(veggies, id: \.self) { veg in
+                        Text(veg.capitalized)
+                        
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move(from:to:))
+                }
             }
-            
+            .accentColor(.blue)
+            .listStyle(.sidebar)
+            .navigationTitle("Grocery List")
+            .navigationBarItems(leading: EditButton(),trailing: addButton)
+        }
+        .accentColor(.red)
+    }
+    
+    var addButton: some View {
+        Button("Add", action: {
+            add()
+        })
+    }
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
+        veggies.remove(atOffsets: indexSet)
+
+    }
+    
+    func move(from: IndexSet, to : Int) {
+        veggies.move(fromOffsets: from, toOffset: to)
+        veggies.move(fromOffsets: from, toOffset: to)
+
+    }
+    
+    func add() {
+        fruits.append("Coconut")
+        veggies.append("LadyFinger")
+
+    }
+}
+
+struct DetailView: View {
+//    @Binding var fruit: String
+//    @Binding var fruitImg: UIImage
+
+    var body: some View {
+        VStack {
+            Image(systemName: "gear")
+            Text("fruit")
         }
     }
 }
