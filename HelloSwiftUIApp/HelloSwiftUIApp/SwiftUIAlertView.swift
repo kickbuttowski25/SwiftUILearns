baa344187674922d04633bf06f9f7f4a6d6385d8
kickbuttowski25 <vishalsinghbisht25@gmail.com>
2024-01-01 20:56:49 +0530

    Some changes on SwiftUIAlertView.

--- HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIAlertView.swift
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIAlertView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct SwiftUIAlertView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Text("Hello")
+                Text("World!")
+            }
+        }
     }
 }
 
