06ce0a075ece76e6e7347d7194fbfc0389a0bc6a
kickbuttowski25 <vishalsinghbisht25@gmail.com>
2024-01-01 21:01:55 +0530

    Some changes

--- HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIAlertView.swift
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIAlertView.swift
@@ -7,15 +7,65 @@
 
 import SwiftUI
 
+enum MyAlerts {
+    case success
+    case error
+}
 struct SwiftUIAlertView: View {
+    
+    @State var showAlert: Bool = false
+    @State var alertTitle = ""
+    @State var alertMsg = ""
+    @State var myAlerts:  MyAlerts = .success
+    @State var backgroundColor: Color = .yellow
+    
     var body: some View {
         ZStack {
-            VStack {
-                Text("Hello")
-                Text("World!")
-                Image(systemName: "heart.fill")
+            backgroundColor.edgesIgnoringSafeArea(.all)
+            HStack {
+                Button("Show Error") {
+                    myAlerts = .error
+//                    alertTitle = "Some error occured"
+//                    alertMsg = "There is some error occured"
+                    showAlert = true
+                }
+                .padding(16)
+                .background(.red)
+                .foregroundColor(.white)
+                .cornerRadius(12)
+                
+                Button("Show Confirm") {
+                    myAlerts = .success
+//                    alertTitle = "Confirmd"
+//                    alertMsg = "The Message Confirmed"
+                    showAlert = true
+                }
+                .padding(16)
+                .background(.green)
+                .foregroundColor(.white)
+                .cornerRadius(12)
+            }
+            .alert(isPresented: $showAlert) {
+                getAlert()
             }
         }
+    }
+    
+    func getAlert() -> Alert {
+        switch myAlerts {
+        case .success:
+          
+            return Alert(title:
+                            Text("This is a success!"),
+                         dismissButton: .default(Text("Ok")))
+        case .error:
+            return Alert(title:
+                                    Text("Some error occured"))
+//        default :
+//            return Alert(title:
+//                            Text("Error"))
+        }
+       
     }
 }
 
