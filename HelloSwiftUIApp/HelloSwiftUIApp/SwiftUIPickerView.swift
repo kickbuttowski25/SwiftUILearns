1e27221529189b6312d4474d6995c0cd4d12a856
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2024-01-07 23:52:12 +0530

    Added Different style Picker view

--- HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIPickerView.swift
+++ HelloSwiftUIApp/HelloSwiftUIApp/SwiftUIPickerView.swift
@@ -2,14 +2,54 @@
 //  SwiftUIPickerView.swift
 //  HelloSwiftUIApp
 //
-//  Created by Vishal Singh Bisht on 06/01/24.
+//  Created by Vishal Singh Bisht on 07/01/24.
 //
 
 import SwiftUI
 
 struct SwiftUIPickerView: View {
+    @State var selection: String = "1"
+    @State var components: [String] = ["Many","Some","Any"]
+    
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = .red
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Picker("Picker", selection: $selection) {
+            ForEach(components.indices) { index in
+                Text(components[index])
+                    .tag(components[index])
+            }
+        }
+        .pickerStyle(.segmented)
+//        Menu("Filter: \(selection)") {
+//            Picker(selection: $selection) {
+//                ForEach(["1","2","3"], id: \.self) { opt in
+//                    HStack {
+//                        Text(opt)
+//                        Image(systemName: "heart.fill")
+//                    }
+//                    .tag(opt)
+//                          }
+//            } label: {
+//                Text("")
+//            }
+//        }
+        
+//        VStack {
+//            HStack {
+//                Text("Age:")
+//                Text(selection)
+//            }
+//            Picker("Picker", selection: $selection) {
+//                ForEach(18..<100) { num in
+//                    Text("\(num)").tag("\(num)")
+//                }
+//
+//            }
+//            //.pickerStyle(.inline)
+//            .pickerStyle(.menu)
+//        }
     }
 }
 
